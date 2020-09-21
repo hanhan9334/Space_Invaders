5c635a06146add59a5b794a30c72c7102527c3cb
hanhan9334 <lhtony9334@126.com>
2020-09-20 20:58:18 -0400

    Finished project and commmited

---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame
+import os
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+    def __init__(self, ai_settings, screen):
+        """Initialize the ship and set its starting position."""
+        super().__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+        # Load the ship image and get its rect.
+        package_dir = os.path.dirname(os.path.abspath(__file__))
+        filename = os.path.join(package_dir, 'images/ship.bmp')
+        self.image = pygame.image.load(filename)
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = self.screen_rect.centerx
